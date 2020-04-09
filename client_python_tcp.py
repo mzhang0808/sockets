6c78fc18c52799b5245435ef0ddf63a88671a970
Michael Zhang <michaelzhang@Michaels-MacBook-Pro.local>
2020-04-08 23:13:07 -0700

    Modularized code

--- client_python_tcp.py
+++ client_python_tcp.py
@@ -1,81 +1,92 @@
 import sys
 import socket
 
-while True:
-    # Take IP as input
-    IP = input("Enter server name or IP address:")
+def validateIP(IP):
     # Validate IP in family ipV4
     try:
         socket.inet_aton(IP)
-        break
+        return True
     except socket.error:
         print("Could not connect to server.")
+        return False
+
+def validatePort(port):
+    # Check that it's int
+    try: 
+        port = int(port)
+    except Exception:
+        print("Invalid port number.")
+        return False
+    # Check that it's in bounds
+    if port > 65535 or port < 0:
+        return False
+    return True
+
+def testConnection(s, IP, port):
+    # Try connection to given IP and Port - if server not listening on same port, exit
+    try:
+        s.connect((IP, port))
+        return True
+    except Exception:
+        print("Could not connect to server.")
+        return False
+
+def sendCommand(s, command):
+    if command.find(" > ") == -1:
+        command = command + " > " + command + ".txt"
+
+    # Send the command to server 
+    s.send(command.encode('utf-8'))
+
+    # Receive stream of data back
+    l = s.recv(1024)
+
+    temp = command.split(" > ")
+
+    filename = temp[1]
+
+    # Check first line sent from server [either error message or first line from file]
+    firstLine = l.decode('utf-8')
+    # Error - no response from server
+    if firstLine == "Did not receive response.":
+        print("Did not receive response.")
+        return False
+
+    # Write to local file
+    f = open(filename, 'w')
+
+    # Write stream to a .txt file
+    while (l):
+        f.write(l.decode('utf-8')) 
+        l = s.recv(1024)
+
+    # Finished reading file
+    f.close()
+    print ("File ", filename, " saved.")
+    s.close()                # Close the connection
+    return True
+
+while True:
+    IP = input("Enter server name or IP address:")
+    if validateIP(IP):
+        break
 
 while True:
     # Take port # as input
     port = input("Enter port:")
-    # Check that it's int
-    try: 
+    if validatePort(port):
         port = int(port)
         break
-    except Exception:
-        print("Invalid port number.")
-
-# Check valid port number
-while port > 65535 or port < 0:
-    print("Invalid port number.")
-    port = input("Enter port:")
-    while True:
-        # Ensure inputted port is #
-        try:
-            port = int(port)
-            break
-        except Exception:
-            print("Invalid port number.")
 
 # Input Command - can be in form "[command]" or "[command] > [filename.txt]"
 command = (input("Enter command:"))
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Try connection to given IP and Port - if server not listening on same port, exit
-try:
-    s.connect((IP, port))
-except Exception:
-    print("Could not connect to server.")
+if not testConnection(s, IP, port):
     sys.exit()
 
-if command.find(" > ") == -1:
-    command = command + " > " + command + ".txt"
-
-# Send the command to server 
-s.send(command.encode('utf-8'))
-
-# Receive stream of data back
-l = s.recv(1024)
-
-temp = command.split(" > ")
-
-filename = temp[1]
-
-# Check first line sent from server [either error message or first line from file]
-firstLine = l.decode('utf-8')
-# Error - no response from server
-if firstLine == "Did not receive response.":
-    print("Did not receive response.")
-    sys.exit()
-
-# Write to local file
-f = open(filename, 'w')
-
-# Write stream to a .txt file
-while (l):
-    f.write(l.decode('utf-8')) 
-    l = s.recv(1024)
-
-# Finished reading file
-f.close()
-print ("File ", filename, " saved.")
-s.close()                # Close the connection
+if not sendCommand(s, command):
+    s.close()
 
 
--- server_python_tcp.py
+++ server_python_tcp.py
@@ -1,6 +1,19 @@
 import subprocess
 import socket
 
+def transferFile(c):
+    # Open file that command's output was stored in
+    f = open("output.txt", 'r')
+    l = f.read(1024)
+    
+    # Read output back to client (potential multiple segments if size(line) > 1024)
+    while(l):
+        c.send(l.encode('utf-8'))
+        l = f.read(1024)
+    #Tell client finished reading and shutdown the socket
+    print("Successful File Transmission")
+    c.shutdown(socket.SHUT_RDWR)
+
 PORT = 3000
 
 # Creates socket instance with address family ipv4 and TCP protocol
@@ -31,20 +44,10 @@ while True:
         out = subprocess.check_output(command, shell=True)
     # Any error will mean the command did not execute successfully - send a no response message to client
     # signifying the command failed                       
-    except subprocess.CalledProcessError as grepexc:                                                                                                   
-        l = "Did not receive response."
+    except subprocess.CalledProcessError as grepexc:
         c.send("Did not receive response.".encode('utf-8'))
         continue
 
-    # Open file that command's output was stored in
-    f = open("output.txt", 'r')
-    l = f.read(1024)
+    transferFile(c)
     
-    # Read output back to client (potential multiple segments if size(line) > 1024)
-    while(l):
-        c.send(l.encode('utf-8'))
-        l = f.read(1024)
-    #Tell client finished reading and shutdown the socket
-    print("Successful File Transmission")
-    c.shutdown(socket.SHUT_RDWR)
 s.close()
